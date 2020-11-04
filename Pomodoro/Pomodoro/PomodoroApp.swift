fc1514457631827be3b4dbc5650ca4b0bc8924d6
aki-it <yoshiaki.sato.20@gmail.com>
2020-11-04 18:22:57 +0900

    first commit

---
+++ Pomodoro/Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Yoshiaki Sato on 2020/11/04.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
